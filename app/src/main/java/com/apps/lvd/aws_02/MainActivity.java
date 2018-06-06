530766965659f9cf31992fb56938642b29c94a3f
donglv1 <kr4591@gmail.com>
2018-06-06 22:13:47 +0700

    Add files via upload
    sample app

---
+++ app/src/main/java/com/apps/lvd/aws_02/MainActivity.java
@@ -0,0 +1,313 @@
+package com.apps.lvd.aws_02;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.handlers.RequestHandler2;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.util.IOUtils;
+import com.apps.lvd.aws_02.model.nosql.DocDO;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private final String KEY = "AKIAIYM647GUXVRT3J4A";
+    private final String SECRET = "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";
+
+    private AmazonS3Client s3Client;
+    private BasicAWSCredentials credentials;
+
+    //track Choosing Image Intent
+    private static final int CHOOSING_IMAGE_REQUEST = 1234;
+
+    private TextView tvFileName;
+    private ImageView imageView;
+    private EditText edtFileName;
+
+    private Uri fileUri;
+    private Bitmap bitmap;
+    DynamoDBMapper dynamoDBMapper;
+    AmazonDynamoDBClient dbClient;
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.img_file);
+        edtFileName = findViewById(R.id.edt_file_name);
+        tvFileName = findViewById(R.id.tv_file_name);
+        tvFileName.setText("");
+
+        findViewById(R.id.btn_choose_file).setOnClickListener(this);
+        findViewById(R.id.btn_upload).setOnClickListener(this);
+        findViewById(R.id.btn_download).setOnClickListener(this);
+
+        AWSMobileClient.getInstance().initialize(this).execute();
+
+        credentials = new BasicAWSCredentials(KEY, SECRET);
+        s3Client = new AmazonS3Client(credentials);
+        dbClient = new AmazonDynamoDBClient(credentials);
+        dynamoDBMapper = new DynamoDBMapper(dbClient);
+        /*AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+
+        AmazonDynamoDBClient dynamoDBClient = new AmazonDynamoDBClient(new DefaultAWSCredentialsProviderChain());
+        RequestHandler2 requestHandler = new FaultInjectionRequestHandler(dynamoDBClient);
+
+        dynamoDBClient.addRequestHandler(requestHandler);*/
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+
+        if (i == R.id.btn_choose_file) {
+            //showChoosingFile();
+            createDocs();
+            readDocs();
+
+        } else if (i == R.id.btn_upload) {
+            uploadFile();
+        } else if (i == R.id.btn_download) {
+            downloadFile();
+        }
+    }
+
+    private void readDocs() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //DynamoDBMapper mapper = new DynamoDBMapper(dbClient);
+                DocDO docDO = dynamoDBMapper.load(
+                        DocDO.class,
+                        "User1",
+                        "Article1");
+
+                // Item read
+                // Log.d("News Item:", newsItem.toString());
+                /*ListTablesResult list = dbClient.listTables();
+                System.out.println(list);*/
+            }
+        }).start();
+    }
+
+    public void createDocs() {
+        final DocDO docsItem = new DocDO();
+
+        docsItem.setUserId("User3");
+
+        docsItem.setName("Article3");
+        docsItem.setTitle("This is the article content");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                dynamoDBMapper.save(docsItem);
+                // Item saved
+            }
+        }).start();
+    }
+
+    private void uploadFile() {
+
+        if (fileUri != null) {
+            final String fileName = edtFileName.getText().toString();
+
+            if (!validateInputFileName(fileName)) {
+                return;
+            }
+
+            final File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    "/" + fileName);
+            //final File file = new File("/storage/emulated/0/Android/data/com.apps.lvd.aws_01/files/Pictures/IMG_20180526_164146-31462874.jpg");
+
+            createFile(getApplicationContext(), fileUri, file);
+
+            TransferUtility transferUtility =
+                    TransferUtility.builder()
+                            .context(getApplicationContext())
+                            .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                            .s3Client(s3Client)
+                            .build();
+
+            TransferObserver uploadObserver =
+                    transferUtility.upload("jsaS3/" + fileName + "." + getFileExtension(fileUri), file);
+
+            uploadObserver.setTransferListener(new TransferListener() {
+
+                @Override
+                public void onStateChanged(int id, TransferState state) {
+                    if (TransferState.COMPLETED == state) {
+                        Toast.makeText(getApplicationContext(), "Upload Completed!", Toast.LENGTH_SHORT).show();
+
+                        file.delete();
+                    } else if (TransferState.FAILED == state) {
+                        file.delete();
+                    }
+                }
+
+                @Override
+                public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                    float percentDonef = ((float) bytesCurrent / (float) bytesTotal) * 100;
+                    int percentDone = (int) percentDonef;
+
+                    tvFileName.setText("ID:" + id + "|bytesCurrent: " + bytesCurrent + "|bytesTotal: " + bytesTotal + "|" + percentDone + "%");
+                }
+
+                @Override
+                public void onError(int id, Exception ex) {
+                    ex.printStackTrace();
+                }
+
+            });
+        }
+    }
+
+    private void downloadFile() {
+        if (fileUri != null) {
+
+            final String fileName = edtFileName.getText().toString();
+
+            if (!validateInputFileName(fileName)) {
+                return;
+            }
+
+            try {
+                final File localFile = File.createTempFile("images", getFileExtension(fileUri));
+
+                TransferUtility transferUtility =
+                        TransferUtility.builder()
+                                .context(getApplicationContext())
+                                .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                                .s3Client(s3Client)
+                                .build();
+
+                TransferObserver downloadObserver =
+                        transferUtility.download("jsaS3/" + fileName + "." + getFileExtension(fileUri), localFile);
+
+                downloadObserver.setTransferListener(new TransferListener() {
+
+                    @Override
+                    public void onStateChanged(int id, TransferState state) {
+                        if (TransferState.COMPLETED == state) {
+                            Toast.makeText(getApplicationContext(), "Download Completed!", Toast.LENGTH_SHORT).show();
+
+                            tvFileName.setText(fileName + "." + getFileExtension(fileUri));
+                            Bitmap bmp = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            imageView.setImageBitmap(bmp);
+                        }
+                    }
+
+                    @Override
+                    public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                        float percentDonef = ((float) bytesCurrent / (float) bytesTotal) * 100;
+                        int percentDone = (int) percentDonef;
+
+                        tvFileName.setText("ID:" + id + "|bytesCurrent: " + bytesCurrent + "|bytesTotal: " + bytesTotal + "|" + percentDone + "%");
+                    }
+
+                    @Override
+                    public void onError(int id, Exception ex) {
+                        ex.printStackTrace();
+                    }
+
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "Upload file before downloading", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void showChoosingFile() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Image"), CHOOSING_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (bitmap != null) {
+            bitmap.recycle();
+        }
+
+        if (requestCode == CHOOSING_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            fileUri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), fileUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private boolean validateInputFileName(String fileName) {
+
+        if (TextUtils.isEmpty(fileName)) {
+            Toast.makeText(this, "Enter file name!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void createFile(Context context, Uri srcUri, File dstFile) {
+        try {
+            InputStream inputStream = context.getContentResolver().openInputStream(srcUri);
+            if (inputStream == null) return;
+            OutputStream outputStream = new FileOutputStream(dstFile);
+            IOUtils.copy(inputStream, outputStream);
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/apps/lvd/aws_02/model/nosql/DocDO.java
@@ -0,0 +1,48 @@
+package com.apps.lvd.aws_02.model.nosql;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "aws-mobilehub-1410684429-doc")
+
+public class DocDO {
+    private String _userId;
+    private String _name;
+    private String _title;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId(final String _userId) {
+        this._userId = _userId;
+    }
+    @DynamoDBRangeKey(attributeName = "name")
+    @DynamoDBAttribute(attributeName = "name")
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(final String _name) {
+        this._name = _name;
+    }
+    @DynamoDBAttribute(attributeName = "title")
+    public String getTitle() {
+        return _title;
+    }
+
+    public void setTitle(final String _title) {
+        this._title = _title;
+    }
+
+}
